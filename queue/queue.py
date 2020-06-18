5717b007608facbf89ff44cf5d703f3fdb8a818e
dang lu <danglu@dangs-mbp.lan>
2020-06-17 18:27:38 -0700

    mvp for day 1 done

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,5 @@
+from singly_linked_list import *
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -13,16 +15,61 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
+# using a list as the underlying storage structure
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.insert(0, value)
+#         self.size +=1
+
+#     def dequeue(self):
+#         if len(self.storage) > 0:
+#             dq = self.storage.pop()
+#             self.size -= 1
+#             return dq
+#         else:
+#             return None
+
+
+# using a linked list as storage structure
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
-
+        print(self.size)
+        return self.size    
+        
     def enqueue(self, value):
-        pass
-
+        new_node = Node(value)
+        if self.size == 0:
+            self.storage.add_to_head(value)
+            self.size +=1
+        
+        else:
+            self.storage.add_to_tail(value)
+            self.size +=1
+    
     def dequeue(self):
-        pass
+        if self.size ==0:
+            return None
+        
+        else:
+            head_val = self.storage.head.value
+            self.storage.remove_head()
+            self.size -= 1
+            return head_val
+        
+
+
--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,6 @@
+from singly_linked_list import *
+
+
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -10,16 +13,48 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+# solution using array as underlying storage structure
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) > 0:
+#             last = self.storage.pop()
+#             return last
+#         else:
+#             return None
+
+# solution using LiknedList as underlying storage structure
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
-
+        return self.size
+    
     def push(self, value):
-        pass
+        self.storage.add_to_head(value)
+        self.size +=1
+
 
     def pop(self):
-        pass
+        if self.size > 0:
+            pop_val = self.storage.head.value
+            self.storage.remove_head()
+            self.size -=1
+            return pop_val
+        else:
+            return None
+
